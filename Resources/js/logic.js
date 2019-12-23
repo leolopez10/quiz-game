80dfee59ce07bd112c64ff749e56d06b71759850
lil L <57925562+leolopez10@users.noreply.github.com>
2019-12-23 14:57:27 -0600

    created multiple functions for saving highscores and ending the quiz

--- Resources/js/logic.js
+++ Resources/js/logic.js
@@ -10,7 +10,7 @@ var titleScreen = document.querySelector("#title-section");
 var quizScreen = document.querySelector("#quiz-section");
 var highScoreScreen = document.querySelector("#highscore-section");
 var highScoreDisplay = document.querySelector("#highscore-display-section");
-var initials = document.querySelector("#initials");
+var initialsEl = document.querySelector("#initials");
 var feedback = document.querySelector("#feedback");
 
 var questionsEl = document.querySelector("#question");
@@ -24,7 +24,7 @@ function startQuiz() {
     titleSection.setAttribute("class", "hide");
   
     // un-hide questions section
-    questionsEl.setAttribute("class", "hide");
+    quizScreen.setAttribute("class", "show");
   
     // start timer
     timerId = setInterval(tick, 1000);
@@ -74,6 +74,107 @@ function startQuiz() {
       choicesEl.appendChild(choiceNode);
     });
   }
+
+// click on question answer either generate new question or end quiz if final question, and deduct time for answering wrong
+  function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      // penalize time
+      time -= 15;
   
+      if (time < 0) {
+        time = 0;
+      }
+  
+      // display new time on page
+      timerEl.textContent = time;
+  
+  
+      feedbackEl.textContent = "Wrong!";
+    } else {
+      // play "right" sound effect
+      sfxRight.play();
+  
+      feedbackEl.textContent = "Correct!";
+    }
+  
+    // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // move to next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+
+
+// end the quiz function
+  function quizEnd() {
+    // stop timer
+    clearInterval(timerId);
+  
+    // show end screen
+    var highscoreSectionEl = document.querySelector("#highscore-section");
+    highscoreSectionEl.setAttribute("class", "show");
+  
+    // show final score
+    var finalScoreEl = document.querySelector("#final-score");
+    finalScoreEl.textContent = time;
+  
+    // hide questions section
+    quizScreen.setAttribute("class", "hide");
+  }
+
+// function for saving highscore
+function saveHighscore() {
+    // get value of input box
+    var initials = initialsEl.value.trim();
+  
+    // make sure value wasn't empty
+    if (initials !== "") {
+      // get saved scores from localstorage, or if not any, set to empty array
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+      // format new score object for current user
+      var newScore = {
+        score: time,
+        initials: initials
+      };
+  
+      // save to localstorage
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+  
+      // redirect to next page
+      window.location.href = "highscores.html";
+    }
+  }
+
+  function checkForEnter(event) {
+    // "13" represents the enter key
+    if (event.key === "Enter") {
+      saveHighscore();
+    }
+  }
+  
+  // user clicks button to submit initials
+  submitBtn.onclick = saveHighscore;
+  
+  // user clicks button to start quiz
+  startBtn.onclick = startQuiz;
+  
+  initialsEl.onkeyup = checkForEnter;
+
+
+
 
 
